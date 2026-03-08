1f3e20a337dd6a350f2cc6dc04a2fb59e5f0c4d4
TZL <hentaikamen0221@gmail.com>
2026-03-08 11:35:17 +0900

    Fix Artheon reset and provide button activation rules

---
+++ Assets/Apps/BLUE/Runtime/Drink/Data/DrinkData.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace PPP.BLUE.VN.DrinkSystem
+{
+    [Serializable]
+    public sealed class DrinkData
+    {
+        public string id;
+        public string name;
+        public Dictionary<string, int> ingredients = new Dictionary<string, int>(StringComparer.Ordinal);
+        public List<string> category = new List<string>();
+        public List<string> tags = new List<string>();
+        public bool artheon_addable;
+        public int total;
+    }
+}
