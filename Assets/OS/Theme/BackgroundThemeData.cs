2628c4360238a38a3096e5b0c86b0a2e890cd586
TZL <hentaikamen0221@gmail.com>
2026-04-04 21:21:25 +0900

    Add composable 3-layer background customization

---
+++ Assets/OS/Theme/BackgroundThemeData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "OS/Background Theme Data", fileName = "BackgroundThemeData")]
+public class BackgroundThemeData : ScriptableObject
+{
+    public Sprite[] skyOptions;
+    public Sprite[] buildingOptions;
+    public Sprite[] highlightOptions;
+}
